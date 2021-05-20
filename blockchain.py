4ab2000dbb6766f17e8f9fc64ad04c95e6055890
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-05-20 14:45:36 -0400

    Improve Blockchain Functionality
    - Moved static methods to separate file (crypto.py)
    
    - Blockchain is now stored on disk & updated as new blocks are mined
    
    - documentation improvements
    
    Co-Authored-By: Dhyey-Mehta <37392632+Dhyey-Mehta@users.noreply.github.com>
    Co-Authored-By: George Shao <GeorgeShao123@gmail.com>
    Co-Authored-By: Eric <43616400+ericj22@users.noreply.github.com>
    Co-Authored-By: ernest1027 <36519873+ernest1027@users.noreply.github.com>
    Co-Authored-By: varGeneric <16168572+varGeneric@users.noreply.github.com>

--- blockchain.py
+++ blockchain.py
@@ -1,39 +1,42 @@
-import hashlib
 import json
 import sys
 
 from time import time
-from uuid import uuid4
-from binascii import hexlify, unhexlify
+
+from crypto import hash
 
 
 class Blockchain(object):
     @property
-    def last_block(self):
+    def last_block(self) -> dict:
         # Returns the last (header) block in the chain
         return self.chain[-1]
 
     @property
-    def blockchain(self):
+    def blockchain(self) -> list:
         return self.chain
 
-    def __init__(self, chain_file=''):
+    def __init__(self, chain_file):
         self.chain = []
         self.current_transactions = []
-
         # If chain already exists on disk
         if chain_file:
             try:
                 with open(chain_file, 'r') as blockchain_file:
                     self.chain = json.loads(blockchain_file.read())
-                return
+                return None
             except (FileNotFoundError, json.JSONDecodeError):
-                print('Error while importing chain_file! Continuing anyways with an empty chain...', file=sys.stderr)
+                print('Error while importing chain_file! Creating temporary test chain', file=sys.stderr)
+                """
+                Create Testchain with default Genesis Block
+                THIS SHOULD NEVER BE CALLED!!
+                    - When rolled out, this functions should instead call for a sync with the node to import the live
+                        chain
+                """
 
-        # Genesis Block
-        self.new_block(previous_hash=1, proof=100)
+                self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self, proof, previous_hash=None):
+    def new_block(self, proof, previous_hash=None) -> dict:
         """
         Creates new block to add to the blockchain. Uses previous header to generate new block.
         :param proof: <int> POW value that returns hash < predetermined target hash
@@ -42,15 +45,9 @@ class Blockchain(object):
         """
         block = {
             'index': len(self.chain) + 1,
-            'timestamp': time(),
             'transactions': self.current_transactions,
-            'previous_hash': previous_hash or hexlify(self.hash(self.last_block)),
+            'previous_hash': hash(self.last_block)
         }
-        self.current_transactions = []
-        # Reset the current list of transactions
-        self.current_transactions = []
-        block['proof'] = proof
-        self.chain.append(block)
         return block
 
     def new_transaction(self, sender, recipient, amount):
@@ -63,7 +60,7 @@ class Blockchain(object):
         :param amount: <float> Amount of $TR (Robcoin) to be transacted
         :return: <int> Index of transaction to add to the new block. (see example-block.py)
         """
-        self.current_transactions.append(
+        self.pending_transactions.append(
             {
                 'sender': sender,
                 'recipient': recipient,
@@ -72,41 +69,9 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        """
-        Hashes a block
-        :param block: <dict> block (see example-block.py)
-        :return: <bytes> hash of block
-            - Returns block hash as bytes
-        """
-        block_encoded = bytes(json.dumps(block, sort_keys=True).encode())  # Sorted to ensure consistent hashes
-        return hashlib.sha256(block_encoded).digest()
-
-    @staticmethod
-    def valid_proof(last_hash, proof):
-        """
-        Validates the Proof.
-        :param last_hash: <bytes> Previous hash
-        :param proof: <int> current proof
-        :return: <bool> true the guessed POW meets the POW criteria (Guessed hash < POW requirement), false if not
-                Guess hash is represented as a binary string.
-                When inverted, the binary hash must have N leading 0s
-                https://en.bitcoin.it/wiki/Block_hashing_algorithm
-        """
-
-        """Difficulty N increases mining difficulty exponentially (2**N)"""
-        N = 5  # Difficulty (N >= 1)
-        guess = f'{last_hash.hex()}{proof}'  # Append proof to end of block hash
-        guess_hash = hashlib.sha256(guess.encode()).hexdigest()
-        bin_guess_hash = f"{bin(int.from_bytes(unhexlify(guess_hash), byteorder='big'))}"[::-1]  # Reverse binary string
-        if proof < 5:
-            print(f'proof={proof}: {bin_guess_hash}')
-            print(f'{last_hash.hex()}{proof}')
-        if bin_guess_hash[:N] == "0" * N:
-            print(f'proof={proof}: {bin_guess_hash}')
-        return bin_guess_hash[:N] == "0" * N
-
     def save_blockchain(self, file="blockchain.txt"):
-        with open('blockchain.txt', 'w') as out:
-            json.dump(self.chain, out)
+        """
+        :param file: saves blockchain to 'blockchain.txt' in case node goes down
+        """
+        with open('blockchain.txt', 'w', encoding='utf-8') as out:
+            json.dump(self.chain, out, ensure_ascii=False, indent=4)  # Padding to make json look pretty
---
+++ crypto.py
@@ -0,0 +1,36 @@
+import hashlib
+import json
+from binascii import unhexlify
+
+
+def hash(block) -> str:
+    """
+    Hashes a block
+    :param block: <dict> block (see example-block.py)
+    :return: <bytes> hash of block
+        - Returns block hash as bytes
+    """
+    block_encoded = bytes(json.dumps(block, sort_keys=True).encode())  # Sorted to ensure consistent hashes
+    return hashlib.sha256(block_encoded).hexdigest()
+
+
+def valid_proof(block, proof, node=False) -> bool:
+    """
+    Validates the Proof.
+    :param current_hash: <bytes> Previous hash
+    :param proof: <int> current proof
+    :return: <bool> true the guessed POW meets the POW criteria (Guessed hash < POW requirement), false if not
+            Guess hash is represented as a binary string.
+            When inverted, the binary hash must have N leading 0s
+            https://en.bitcoin.it/wiki/Block_hashing_algorithm
+    """
+
+    """Difficulty N increases mining difficulty exponentially (2**N)"""
+    N: int = 16  # Difficulty (N >= 1)
+
+    block['proof'] = proof
+    guess = block
+    # Append proof to end of block hash
+    guess_hash = hash(guess)
+    bin_guess_hash = f"{bin(int.from_bytes(unhexlify(guess_hash), byteorder='big'))}"[::-1]  # Reverse binary string
+    return bin_guess_hash[:N] == "0" * N
